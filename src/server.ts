dffbaa56c8033005e64f2fa33eb1ed1cd7ed8d8a
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-13 13:16:10 -0300

    Configure route to create orphanages

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,16 @@ import 'dotenv/config';
 import express from 'express';
 
 import './database';
+
+import OrphanagesController from './controllers/OrphanagesController';
+import orphanageValidator from './validators/orphanageValidator';
+
 const app = express();
+
+const orphanagesController = new OrphanagesController();
+
+app.use(express.json());
+
+app.post('/orphanages', orphanageValidator, orphanagesController.store);
 
 app.listen(process.env.APP_PORT);
