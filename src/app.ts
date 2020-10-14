e382af3a6a1b273960173a6e27c0d983b0bab22c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-13 23:33:34 -0300

    Separate server and app

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import 'express-async-errors';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import { errors } from 'celebrate';
+
+import './database';
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use('/v1', routes);
+
+app.use(errors());
+app.use(errorHandler);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,3 @@
-import 'dotenv/config';
-import 'express-async-errors';
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
-import helmet from 'helmet';
-import { errors } from 'celebrate';
-
-import './database';
-import routes from './routes';
-import errorHandler from './errors/handler';
-
-const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.use(routes);
-
-app.use(errors());
-app.use(errorHandler);
+import app from './server';
 
 app.listen(process.env.APP_PORT);
