90918ba11179ad47e62a3712b18fd4656709e403
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-14 20:09:48 -0300

    Update database configuration file

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: 'default',
+    type: 'sqlite',
+    database: './src/database/database.sqlite',
+    migrations: ['./src/database/migrations/*.ts'],
+    entities: ['./src/models/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+];
