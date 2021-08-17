18ef4be00f15a4f44f18e1196432d3930921fccc
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-17 19:23:04 -0300

    Create docs endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
+import swagger from 'swagger-ui-express';
 
 import './database';
+
+import swaggerDocument from './swagger.json';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
@@ -15,7 +18,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
 app.use('/v1', routes);
 
 app.use(errors());
