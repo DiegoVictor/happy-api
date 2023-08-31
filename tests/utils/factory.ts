d081a6ebaf3537ad362b3e9344d71953d61cd9c6
Diego Victor <diegovictorgonzaga@gmail.com>
2023-08-31 00:07:49 -0300

    Update faker usage

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -5,19 +5,19 @@ import Orphanage from '../../src/models/Orphanage';
 import Image from '../../src/models/Image';
 
 factory.define('Orphanage', Orphanage, {
-  name: faker.name.findName,
-  latitude: faker.address.latitude,
-  longitude: faker.address.longitude,
+  name: faker.person.fullName,
+  latitude: faker.location.latitude,
+  longitude: faker.location.longitude,
   about: faker.lorem.sentence,
   instructions: faker.lorem.sentence,
   opening_hours: faker.lorem.sentence,
   open_on_weekends: faker.datatype.boolean,
-  whatsapp: faker.phone.phoneNumber,
+  whatsapp: faker.phone.number,
 });
 
 factory.define('Image', Image, {
-  path: () => faker.random.alphaNumeric(10) + '.jpg',
-  orphanage_id: faker.datatype.number,
+  path: () => faker.string.alphanumeric(10) + '.jpg',
+  orphanage_id: faker.number.int,
 });
 
 export default factory;
