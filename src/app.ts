817f063975958e1f38f670df87565864d588d933
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-02-14 09:21:37 -0300

    Remove swagger endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,9 @@ import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
-import swagger from 'swagger-ui-express';
 
 import './database';
 
-import swaggerDocument from './swagger.json';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
@@ -19,7 +17,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use('/v1', routes);
